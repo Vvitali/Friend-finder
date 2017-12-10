ca34d5c1498882bb3fe02b6818f8a6ca8cc3e1c4
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-12-10 00:34:17 -0500

    basic server

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var DEBUG = process.argv[2];
-var app  = new require("express");
+var app  = new require("express")();
 var body = require("body-parser");
 var path = require("path");
-DEBUG && console.log("Server file!");
+var fs = require("fs");
+DEBUG && console.log("Server file! DEBUG MODE ON");
 
 app.get("/", function(request, response){
-	response.send("<h2>Hello world!</h2>");
-})
+	response.send("<h2>"+process.argv[2]+"</h2>");
+});
+
+app.get("/survey", function(request, response){
+	response.send("<h2>"+"Survey page!"+"</h2>");
+});
+
+app.get("/api/friends/:friend_name", function(request, response){
+	response.send("<h2>"+request.query+"</h2>");
+	
+});
+
+
+
+app.listen(8000);
